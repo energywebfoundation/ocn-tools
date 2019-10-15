b8d7e8b8c071f6d75a6ab5733f80ac97d0548bc1
Adam Staveley <adam.staveley@shareandcharge.com>
2019-10-15 17:08:43 +0200

    update default tariffs

--- src/data/tariffs.ts
+++ src/data/tariffs.ts
@@ -1,5 +1,5 @@
 import { ITariff } from "ocn-bridge/dist/models/ocpi/tariffs";
-import { config } from "../config/config"
+import { config } from "../config/config";
 import { extractCPO } from "../tools/tools";
 
 const cpo = extractCPO(config.cpo.roles)
@@ -11,14 +11,60 @@ export const tariffs: ITariff[] = [
         id: "1",
         currency: "CHF",
         elements: [{
-          price_components: [{
-              type: "ENERGY",
-              price: 0.35,
-              step_size: 1000
-          }],
-          restrictions: {
-              max_kwh: 100
-          }
+            price_components: [
+                {
+                    type: "FLAT",
+                    price: 0.5,
+                    vat: 0.077,
+                    step_size: 1
+                }, 
+                {
+                    type: "ENERGY",
+                    price: 0.25,
+                    vat: 0.077,
+                    step_size: 1000        
+                }
+            ]
+        }],
+        last_updated: new Date()
+    },
+    {
+        country_code: cpo.country_code,
+        party_id: cpo.party_id,
+        id: "2",
+        currency: "CHF",
+        elements: [{
+            price_components: [
+                {
+                    type: "FLAT",
+                    price: 0.5,
+                    vat: 0.077,
+                    step_size: 1
+                }, 
+                {
+                    type: "TIME",
+                    price: 2,
+                    vat: 0.077,
+                    step_size: 900        
+                }
+            ]
+        }],
+        last_updated: new Date()
+    },
+    {
+        country_code: cpo.country_code,
+        party_id: cpo.party_id,
+        id: "3",
+        currency: "CHF",
+        elements: [{
+            price_components: [
+                {
+                    type: "FLAT",
+                    price: 8,
+                    vat: 0.077,
+                    step_size: 1
+                }
+            ]
         }],
         last_updated: new Date()
     }

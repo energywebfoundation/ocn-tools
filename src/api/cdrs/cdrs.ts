34064ee27d77b4ef2ca49b8ecc27be9a6b3845cb
Arzon <arzon>
2019-10-17 20:52:34 +0600

    changes for cdrs sender

--- src/api/cdrs/cdrs.ts
+++ src/api/cdrs/cdrs.ts
@@ -1,5 +1,7 @@
 import { CdrsReceiver } from "./cdrs.receiver";
+import { CdrsSender } from "./cdrs.sender";
 
 export class Cdrs {
     public receiver = new CdrsReceiver()
+    public sender = new CdrsSender()
 }

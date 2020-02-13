3638828a08343b4a69bf2a7eac444b8b5d2e23ee
Arzon <arzon>
2020-02-13 21:42:36 +0600

    added license
    Signed-off-by: Arzon <arzon>

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -1,3 +1,18 @@
+/*
+    Copyright 2019-2020 eMobilify GmbH
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 import { ISession } from "ocn-bridge/dist/models/ocpi/session";
 
 export const sessions: ISession[] = [{
@@ -91,7 +106,7 @@ export const sessions: ISession[] = [{
     },
     status: "COMPLETED",
     last_updated: new Date().toISOString()
-},{
+}, {
     country_code: "CH",
     party_id: "CPO",
     id: "SES0000000005",
@@ -113,7 +128,7 @@ export const sessions: ISession[] = [{
     },
     status: "COMPLETED",
     last_updated: new Date().toISOString()
-},{
+}, {
     country_code: "CH",
     party_id: "CPO",
     id: "SES0000000006",
